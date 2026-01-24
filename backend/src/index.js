9822df7e1952a2c5fb8b74976d8ca8f095b779dd
Imrannazirmim <imrannazirmim22@gmail.com>
2026-01-24 12:22:08 +0600

    setup project

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(
+    cors({
+        origin: "http://localhost:5000",
+        credentials: true,
+    }),
+);
+app.use(express.json());
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectDB();
+});
