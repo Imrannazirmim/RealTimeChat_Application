7e02a943c0e1919717e5e52dad9d724a44e50d08
Imrannazirmim <imrannazirmim22@gmail.com>
2026-03-01 19:27:22 +0600

    fix storage 10 mb added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,28 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 app.use(
-      cors({
-          origin: "http://localhost:5173",
-          credentials: true,
-      }),
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
 );
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use('/api/message', messageRoutes)
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     connectDB();
 });
-
-
